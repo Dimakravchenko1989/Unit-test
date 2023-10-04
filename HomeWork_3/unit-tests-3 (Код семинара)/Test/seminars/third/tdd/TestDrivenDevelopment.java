10fa5f0510622e614998f7ba524bbf3c4b9c0664
Dmitriy <dimakravchenko1989@yandex.ru>
2023-10-05 02:24:20 +0300

    Добавил папку ДЗ-3

---
+++ HomeWork_3/unit-tests-3 (Код семинара)/Test/seminars/third/tdd/TestDrivenDevelopment.java
@@ -0,0 +1,37 @@
+package seminars.third.tdd;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+// Задание №5
+// Примените подход TDD для создания нового класса MoodAnalyser, который оценивает настроение выраженное во фразах.
+
+
+public class TestDrivenDevelopment {
+
+    //3.5. Red-Green-Refactor
+    @Test
+    public void testAnalyseMood_Refactor() {
+        MoodAnalyser moodAnalyser = new MoodAnalyser();
+        String message = "Refactor";
+        String mood = moodAnalyser.analyseMood(message);
+        Assertions.assertEquals("Refactor", mood);
+    }
+
+    @Test
+    public void testAnalyseMood_FallingTest() {
+        MoodAnalyser moodAnalyser = new MoodAnalyser();
+        String message = "Red";
+        String mood = moodAnalyser.analyseMood(message);
+        Assertions.assertEquals("Write a falling test", mood);
+    }
+
+    @Test
+    public void testAnalyseMood_TestPass() {
+        MoodAnalyser moodAnalyser = new MoodAnalyser();
+        String message = "Green";
+        String mood = moodAnalyser.analyseMood(message);
+        Assertions.assertEquals("Make the test pass", mood);
+    }
+}
